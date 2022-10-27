63a4f3fd0ba3695a638b514292a28342a00c22bd
Emil Forsén <emil.forsen@klash.studio>
2022-10-27 15:29:29 +0200

    Updated RandomWalker

--- Programming fundamentals/07 - Random Walker/forsen_emil_random_walker/Assets/Emifor.cs
+++ Programming fundamentals/07 - Random Walker/forsen_emil_random_walker/Assets/Emifor.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Diagnostics;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
@@ -8,108 +10,229 @@ using Vector2 = UnityEngine.Vector2;
 class Emifor : IRandomWalker
 {
     //Add your own variables here.
-    private Vector2 _walkerPosition;
-    private Vector2 _previousWalkerPosition;
-    private float _width;
-    private float _height;
-    private float _buffer;
+    private enum DirectionState
+    {
+        DownLeft,
+        DownRight,
+        UpRight,
+        UpLeft,
+    }
+    
+    private Vector2 walkerPosition;
+    private Vector2 previousWalkerPosition;
+    private float width;
+    private float height;
+    private float buffer = 10;
 
-    private int _counter;
-    private int[] _fibonacci = {4, 1, 4 , 2, 4, 2, 3, 2, 3, 2, 3, 2, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1,};
+    private int counter;
+    private int[] fibonacciDownLeft = {4, 1, 4};
+    private int[] fibonacciDownRight = {2, 4, 2};
+    private int[] fibonacciUpRight = {3, 2, 3};
+    private int[] fibonacciUpLeft = {1, 3, 1};
 
+    private int[] fibonacci = {1, 1, 3, 5, 8, 13, 21};
+    private int[] coordinates;
+    private int coordinate;
+    private int currentFibonacci = 1;
+    private int currentFibonacciIndex = 1;
+    private int step;
+    private bool goingForward = true;
+
+    private DirectionState mirrorState;
+    private DirectionState currentState;
+    private DirectionState nextState;
+    
+    
     public string GetName()
     {
         return "Emil"; //When asked, tell them our walkers name
     }
-
+    
     public Vector2 GetStartPosition(int playAreaWidth, int playAreaHeight)
     {
-        _width = playAreaWidth;
-        _height = playAreaHeight;
+        width = playAreaWidth;
+        height = playAreaHeight;
 
         //Select a starting position or use a random one.
-        float x = _width / 2;
-        float y = _height / 3;
+        float x = width / 2;
+        float y = height / 3;
 
-        _walkerPosition = new Vector2(x, y);
-        _previousWalkerPosition = new Vector2(x, y);
+        walkerPosition = new Vector2(x, y);
+        previousWalkerPosition = new Vector2(x, y);
+        
+        //Creates an array containing a fibonacci sequence
+        // var n = 20;
+        // for (int i = 0; i < n; i++)
+        // {
+        //     fibonacci = new int[n];
+        //     fibonacci[i] = FibonacciSequence(i);
+        //     Debug.Log(fibonacci[i]);
+        // }
+        
+        //Sets the start fibonacci number to one to prevent illegal move of 0
+        currentFibonacciIndex = 1;
+        currentFibonacci = 1;
+        
+        // Sets the start direction to down left
+        currentState = DirectionState.DownLeft;
 
         return new Vector2(x, y);
     }
-    
-    
-    // TODO
-    // First fibonacci = 4, 1, 4
-    // Second fibonacci = 2, 4, 2
-    // Third fibonacci = 3, 2, 3, 2, 3, 2
-    // Fourth fibonacci = 3, 1, 3, 1, 3, 1, 3, 1, 3, 1
-    
-    
-    
+
     public Vector2 Movement()
     {
-        _counter %= _fibonacci.Length;
-        switch (_fibonacci[_counter])
+
+        if (currentState == DirectionState.DownLeft)
         {
-            case 1:
-                _counter++;        
-                return TurnLeft();
-
-            case 2:
-                _counter++;
-                return TurnRight();
-
-            case 3:
-                _counter++;
-                return GoUp();
-
-            case 4:
-                _counter++;
-                return GoDown();
-            
-            default:
-                
-                return new Vector2(0, 0);
+            mirrorState = DirectionState.UpRight;
+            nextState = DirectionState.DownRight;
+            coordinates = fibonacciDownLeft;
+        }
+        else if (currentState == DirectionState.DownRight)
+        {
+            mirrorState = DirectionState.UpLeft;
+            nextState = DirectionState.UpRight;
+            coordinates = fibonacciDownRight;
+        }
+        else if (currentState == DirectionState.UpRight)
+        {
+            mirrorState = DirectionState.DownLeft;
+            nextState = DirectionState.UpLeft;
+            coordinates = fibonacciUpRight;
+        }
+        else 
+        {
+            mirrorState = DirectionState.DownRight;
+            nextState = DirectionState.DownLeft;
+            coordinates = fibonacciUpLeft;
         }
 
+        coordinate = counter % coordinates.Length;
+        Debug.Log("Step " + step);
+        
+        
+        Debug.Log("Current fib " + currentFibonacci);
+        Debug.Log("Index" + currentFibonacciIndex);
+
+        if (counter % 3 == 0)
+        {
+            step++;
+        }
+
+        if (OutOfBounds(width, height, buffer))
+        {
+            Array.Reverse(fibonacci);
+            currentFibonacciIndex = 0;
+            goingForward = !goingForward;
+        }
+
+        if (step == currentFibonacci)
+        {
+            step = 0;
+            currentFibonacciIndex++;
+
+            if (currentFibonacciIndex > fibonacci.Length)
+            {
+                currentFibonacciIndex = 0;
+            }
+            
+            currentFibonacci = fibonacci[currentFibonacciIndex];
+
+            if (goingForward)
+            {
+                currentState = nextState;
+            }
+            else
+            {
+                currentState = mirrorState;
+            }
+        }
+        
+        switch (coordinates[coordinate])
+            {
+                case 1:
+                    counter++;
+                    return TurnLeft();
+
+                case 2:
+                    counter++;
+                    return TurnRight();
+
+                case 3:
+                    counter++;
+                    return GoUp();
+
+                case 4:
+                    counter++;
+                    return GoDown();
+                
+                default:
+                    return new Vector2(0,0);
+            }
     }
 
     private Vector2 TurnLeft()
     {
-        _walkerPosition.x -= 1;
+        walkerPosition.x -= 1;
         return new Vector2(-1, 0);
     }
     
     private Vector2 TurnRight()
     {
-        _walkerPosition.x += 1;
+        walkerPosition.x += 1;
         return new Vector2(1, 0);
     }
 
     private Vector2 GoUp()
     {
-        _walkerPosition.y += 1;
+        walkerPosition.y += 1;
         return new Vector2(0, 1);
     }
 
     private Vector2 GoDown()
     {
-        _walkerPosition.y -= 1;
+        walkerPosition.y -= 1;
         return new Vector2(0, -1);
+    }
+
+    bool OutOfBounds(float width, float height, float buffer)
+    {
+        width -= buffer;
+        height -= buffer;
+        if (walkerPosition.x >= width || walkerPosition.x <= 0 + buffer)
+        {
+            return true;
+        }
+        else if (walkerPosition.y >= height || walkerPosition.y <= 0 + buffer)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     int DirectionSwitch()
     {
-        if (_walkerPosition.y - _previousWalkerPosition.y < 0)
+        if (walkerPosition.y - previousWalkerPosition.y < 0)
         {
-            _previousWalkerPosition = _walkerPosition;
+            previousWalkerPosition = walkerPosition;
             return 1;
         }
         
-        _previousWalkerPosition = _walkerPosition;
+        previousWalkerPosition = walkerPosition;
         return 4;
     }
 
-   
-    
+    int FibonacciSequence(int number)
+    {
+        if (number == 0)
+            return 0;
+        else if (number == 1)
+            return 1;
+        else 
+            return FibonacciSequence(number - 1) + FibonacciSequence(number - 2);
+    }
+
 } 
